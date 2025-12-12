610f8eec252cfbb2886f195a239866abfc0a6314
mnurullahsaglam <nurullahsl87@gmail.com>
2025-12-12 15:13:04 +0300

    update users table

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,11 +13,16 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+
             $table->string('name');
+            $table->string('surname');
             $table->string('email')->unique();
+            $table->string('birth_date');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+
             $table->rememberToken();
+
             $table->timestamps();
         });
 
